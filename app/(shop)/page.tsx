fd5a3f62339a55a4fbf6c6eed2972a2350f7cb5b
Datdev-123 <22H1120066@ut.edu.vn>
2025-06-04 18:30:47 +0700

    Fix Next.js routing conflict

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,19 +1,5 @@
-import { Hero } from "@/components/shop/hero";
-import { FeaturedProducts } from "@/components/shop/featured-products";
-import { Categories } from "@/components/shop/categories";
-import { NewArrivals } from "@/components/shop/new-arrivals";
-import { Testimonials } from "@/components/shop/testimonials";
-import { Newsletter } from "@/components/shop/newsletter";
+import { redirect } from 'next/navigation';
 
-export default function ShopPage() {
-  return (
-    <div className="flex flex-col gap-16 py-8">
-      <Hero />
-      <Categories />
-      <FeaturedProducts />
-      <NewArrivals />
-      <Testimonials />
-      <Newsletter />
-    </div>
-  );
+export default function ShopIndexPage() {
+  redirect('/shop');
 }
